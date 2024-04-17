49b437679f687ad0aed9bfe63b3f47c3a3e96d9c
Mr-AsimGhaffar <asim.ghaffar71@gmail.com>
2024-04-17 16:56:02 +0500

    added todo

---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Todo } from "../model";
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import "./styles.css";
+import { RxCross2 } from "react-icons/rx";
+
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const [icon, setIcon] = useState(true);
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  return (
+    <form className="todos__single">
+      {todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
+
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon">
+          <AiFillDelete />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          {icon ? (
+            <MdDone
+              onClick={() => {
+                setIcon(!icon);
+              }}
+            />
+          ) : (
+            <RxCross2
+              onClick={() => {
+                setIcon(!icon);
+              }}
+            />
+          )}
+        </span>
+      </div>
+    </form>
+  );
+};
+
+export default SingleTodo;
